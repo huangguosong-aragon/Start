807806234fd5cc245265ff5e7656681385ef0ecf
huangguosong <huangguosong@aragoncs.com>
2018-05-29 16:43:21 +0800

    first commit

---
+++ Start/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Start
+//
+//  Created by 黄国颂 on 2018/5/29.
+//  Copyright © 2018年 Winston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Start/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Start
+//
+//  Created by 黄国颂 on 2018/5/29.
+//  Copyright © 2018年 Winston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
